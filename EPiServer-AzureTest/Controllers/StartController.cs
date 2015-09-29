8f30b2ad0383a5065890baaa8afe4d11211c43d5
Lars Smeby <lars.smeby@bekk.no>
2015-09-29 12:17:07 +0200

    Initial commit

---
+++ EPiServer-AzureTest/Controllers/StartController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using EPiServer.Web.Mvc;
+using EPiServer_AzureTest.Models.Pages;
+
+namespace EPiServer_AzureTest.Controllers
+{
+    public class StartController : PageController<StartPage>
+    {
+        public ActionResult Index(StartPage currentPage)
+        {
+            return View(currentPage);
+        }
+    }
+}
---
+++ EPiServer-AzureTest/Models/Pages/StartPage.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Web;
+
+namespace EPiServer_AzureTest.Models.Pages
+{
+    [ContentType(DisplayName = "StartPage", GUID = "6ecf6ebc-65ca-42f9-b928-9e2cb1e7f531", Description = "The start page of the EPiServer Azure test site")]
+    public class StartPage : PageData
+    {
+        [Display(
+            Name = "Velkomsttekst",
+            Order = 10,
+            GroupName = SystemTabNames.Content)]
+        [UIHint(UIHint.LongString)]
+        public virtual string WelcomeMessage { get; set; }
+    }
+}
